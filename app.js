06fd3728aec17173c1a6607b7330f634480dd04a
Mariano Gonzalez <mariano@mariano-desktop.(none)>
2014-08-19 10:37:34 -0300

    testeo de git

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
 // DB.
 var schemas = require('./db/schemas').schemas;
 // Middleware
